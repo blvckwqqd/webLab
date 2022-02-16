0f7e66645c78925d1b8d9bb301739f06183155e4
blvckwqqd <mishagin.2012@mail.ru>
2022-02-16 23:34:56 +0500

    end

--- index.js
+++ index.js
@@ -24,3 +24,20 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+$( function() {
+  $( document ).tooltip();
+  var NumSpinner = $( "#spinner" ).spinner();
+  var StrSpinner = $( "#tags" ).spinner();
+  var availableTags = [
+    "Bickering", "Bum", "Burrowing", "Command", "Pork", 
+    "Portable", "Titus", "Unpack", "Uranium", "Xylography",
+    "Cilia", "Colloidal", "Dine", "Get into", "slab",
+    "Manly", "Mural", "One-row", "Rain", "Rarity", "Tossit"
+  ];
+  $( "#tags" ).autocomplete({
+    source: availableTags
+    });
+  $( "#dp" ).datepicker();
+  $( "#accordion" ).accordion();
+} );
